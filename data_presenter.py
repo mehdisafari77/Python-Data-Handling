9d8d8b04dda29d0fc97df1ae227c537e33970380
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-20 09:05:37 -0800

    finish problem 5 6 7

--- data_presenter.py
+++ data_presenter.py
@@ -9,3 +9,21 @@ for row in data:
 for row in data:
   values = row.split(',')
   print(values[2])
+
+  # Problem 5
+for row in data:
+  values = row.split(',')
+  total = int(values[3]) * float(values[4])
+  print(total)
+
+# Problem 6
+total = 0
+
+for row in data:
+  values = row.split(',')
+  total = total + (int(values[3]) * float(values[4]))
+
+print(total)
+
+# Problem 7
+data.close()
