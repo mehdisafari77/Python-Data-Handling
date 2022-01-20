a5f24451b94776862831a61059ac26af3657f63e
Mehdi Safari <sa.mehdisafari@gmail.com>
2022-01-20 09:06:32 -0800

    done with coding

--- data_presenter.py
+++ data_presenter.py
@@ -27,3 +27,24 @@ print(total)
 
 # Problem 7
 data.close()
+
+import matplotlib.pyplot as plt 
+    
+# x axis values 
+x = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat", "Sun"] 
+# y axis values 
+y = [10,40,32,84,60,52,18] 
+    
+# plotting  points  
+plt.plot(x, y) 
+    
+# naming x axis 
+plt.xlabel('Day Purchased') 
+# naming y axis 
+plt.ylabel('Cupcakes Purchased') 
+    
+# giving a title to graph 
+plt.title('My Cupcake Sales') 
+    
+# function to show plot 
+plt.show() 
